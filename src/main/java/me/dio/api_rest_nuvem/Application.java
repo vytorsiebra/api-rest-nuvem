fa7b975137e060d0bb67ae8815925321bd8dc281
Vytor Siebra <124097352+vytorsiebra@users.noreply.github.com>
2025-03-29 11:43:10 -0300

    Subir projeto no github

---
+++ src/main/java/me/dio/api_rest_nuvem/Application.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/api_rest_nuvem/ApiRestNuvemApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.api_rest_nuvem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestNuvemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
